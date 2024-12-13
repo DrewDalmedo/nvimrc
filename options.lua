046cf2d062c47f896048f3c3f9d4dcf756c6dc86
Drew Dalmedo <andrewdalmedo@gmail.com>
2024-12-13 14:58:04 -0500

    Switch Windows shell from Powershell to Bash

--- options.lua
+++ options.lua
@@ -2,21 +2,18 @@ vim.wo.number = true
 vim.wo.relativenumber = true
 
 vim.opt.expandtab = true
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.termguicolors = false
 
 if vim.g.goneovim then
   vim.o.guifont = "UbuntuMono Nerd Font:h18"
 end
 
--- if on windows, set the default shell from cmd.exe to powershell
+-- if on Windows, set the default shell from cmd.exe to bash
 if vim.fn.has('win32') == 1 then
-    vim.opt.shell = 'powershell'
-    vim.opt.shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;'
-    vim.opt.shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
-    vim.opt.shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
-    vim.opt.shellquote = ''
+    vim.opt.shell = 'bash'
+    vim.opt.shellcmdflag = '--login -i'
     vim.opt.shellxquote = ''
 end
 
