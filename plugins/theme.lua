534de096b56bf37970555d972a1bb17f011f2466
Drew Dalmedo <andrewdalmedo@gmail.com>
2025-05-02 10:45:24 -0400

    remove non-functional plugin config for GUI mode

--- plugins/theme.lua
+++ plugins/theme.lua
@@ -1,13 +1,4 @@
 return {
   "sainnhe/gruvbox-material",
   enabled = true,
-  config = function()
-    if vim.g.goneovim then          -- if in gui use the color theme
-      vim.opt.termguicolors = true
-      vim.cmd([[ color gruvbox-material ]])
-    else
-      vim.opt.termguicolors = false -- use terminal theme
-    end
-
-  end
 }
