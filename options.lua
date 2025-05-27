e474315c90ce062e34ccd4f31e28bee0dd72f154
Drew Dalmedo <andrewdalmedo@github.com>
2025-05-27 02:50:28 -0400

    font scaling keybinds

--- options.lua
+++ options.lua
@@ -7,11 +7,17 @@ vim.opt.shiftwidth = 4
 vim.opt.termguicolors = false
 
 if vim.g.neovide then
-  vim.o.guifont = "UbuntuMono Nerd Font:h18"
-  vim.opt.termguicolors = true
-  vim.cmd([[ color gruvbox-material ]])
-  -- Toggle fullscreen with F11 in Neovide
-  vim.api.nvim_set_keymap('n', '<F11>', [[<Cmd>lua vim.g.neovide_fullscreen = not vim.g.neovide_fullscreen<CR>]], { noremap = true, silent = true })
+    vim.o.guifont = "UbuntuMono Nerd Font:h24"
+    vim.opt.termguicolors = true
+    vim.cmd([[ color gruvbox-material ]])
+
+    -- Toggle fullscreen with F11 in Neovide
+    vim.api.nvim_set_keymap('n', '<F11>', [[<Cmd>lua vim.g.neovide_fullscreen = not vim.g.neovide_fullscreen<CR>]], { noremap = true, silent = true })
+
+    -- dynamic resizing
+    vim.keymap.set({ "n", "v" }, "<C-=>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>")
+    vim.keymap.set({ "n", "v" }, "<C-->", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>")
+    vim.keymap.set({ "n", "v" }, "<C-0>", ":lua vim.g.neovide_scale_factor = 1<CR>")
 end
 
 -- if on Windows, set the default shell from cmd.exe to bash
