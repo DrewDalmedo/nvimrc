bf063a882cecc503f0b35d7ad162ae7a568939a7
Drew Dalmedo <andrewdalmedo@github.com>
2025-05-31 15:59:26 -0400

    use theme in terminal + neovide

--- options.lua
+++ options.lua
@@ -4,12 +4,11 @@ vim.wo.relativenumber = true
 vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.termguicolors = false
+
+vim.opt.termguicolors = true 
 
 if vim.g.neovide then
     vim.o.guifont = "UbuntuMono Nerd Font:h24"
-    vim.opt.termguicolors = true
-    vim.cmd([[ color gruvbox-material ]])
 
     -- Toggle fullscreen with F11 in Neovide
     vim.api.nvim_set_keymap('n', '<F11>', [[<Cmd>lua vim.g.neovide_fullscreen = not vim.g.neovide_fullscreen<CR>]], { noremap = true, silent = true })
