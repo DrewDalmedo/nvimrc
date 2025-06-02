b1fb91ac9a72ed76b552dc3fd99948344afbf44c
Drew Dalmedo <andrewdalmedo@github.com>
2025-06-02 15:33:37 -0400

    misspelled selector oops

--- keybinds.lua
+++ keybinds.lua
@@ -15,7 +15,7 @@ vim.keymap.set("n", "<leader>ff", teleBuiltins.find_files, {})
 vim.keymap.set("n", "<leader>fg", teleBuiltins.live_grep, {})
 vim.keymap.set("n", "<leader>fm", teleBuiltins.man_pages, {})
 -- project selection
-vim.keymap.set("n", "<leader>pd", "<cmd>lua require('drew.custom.project_selecter').ldr_cd()<CR>", { noremap = true, silent = true } )
+vim.keymap.set("n", "<leader>pd", "<cmd>lua require('drew.custom.project_selector').ldr_cd()<CR>", { noremap = true, silent = true } )
 
 -- TERMINAL
 -- NOTE: replaced with glacier.nvim
