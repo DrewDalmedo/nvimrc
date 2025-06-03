a8b6b61b8d51ed679a5a605573c8cc83c3bd3d0a
Drew Dalmedo <andrewdalmedo@gmail.com>
2025-06-03 10:59:57 -0400

    feat: register typst preview command

--- plugins/typst.lua
+++ plugins/typst.lua
@@ -1,5 +1,10 @@
 return {
-  'kaarmu/typst.vim',
-  ft = 'typst',
-  lazy=false,
+  "kaarmu/typst.vim",
+  ft = "typst",
+  lazy = false,
+  config = function()
+    local preview = require("drew.custom.file_preview")
+
+    preview.register("typst", "TypstWatch")
+  end,
 }
