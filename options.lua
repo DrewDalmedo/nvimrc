3a21a0b007d9b5b34a1c06a8b403bfdbe5cf72c3
Drew Dalmedo <andrewdalmedo@gmail.com>
2025-06-03 09:34:53 -0400

    refactor: set tab width to 2

--- options.lua
+++ options.lua
@@ -2,8 +2,8 @@ vim.wo.number = true
 vim.wo.relativenumber = true
 
 vim.opt.expandtab = true
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.wrap = false
 
 vim.opt.autoindent = true   -- on by default in nvim, good to explicitly enable anyways
