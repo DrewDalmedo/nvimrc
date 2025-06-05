741c59f12b83b948000bf9af6dbad80258029d92
Drew Dalmedo <andrewdalmedo@gmail.com>
2025-06-05 01:37:42 -0400

    feat: relative line numbering & navigation in neogit status

--- plugins/neogit.lua
+++ plugins/neogit.lua
@@ -15,7 +15,26 @@ return {
 
     -- configure neogit
     neogit.setup({
-      kind = "replace"
+      kind = "replace",
+
+      -- set up line numbering & overwrite numbered commands
+      disable_line_numbers = false,
+      disable_relative_line_numbers = false,
+
+      mappings = {
+        status = {
+          -- disable the original numbered commands
+          ["1"] = false,  -- Depth1
+          ["2"] = false,  -- Depth2
+          ["3"] = false,  -- Depth3
+          ["4"] = false,  -- Depth4
+          -- assign the numbered commands to a corresponding <c-number> binding
+          ["<c-1>"] = "Depth1",
+          ["<c-2>"] = "Depth2",
+          ["<c-3>"] = "Depth3",
+          ["<c-4>"] = "Depth4",
+        },
+      },
     })
 
     -- keybind
