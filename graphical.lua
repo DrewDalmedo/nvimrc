6b2fac1c7c76d4e126f11dfe00dcf8f18e8cdeb1
Drew Dalmedo <andrewdalmedo@gmail.com>
2025-09-19 17:20:11 -0400

    lower neovide font size

--- graphical.lua
+++ graphical.lua
@@ -1,6 +1,6 @@
--- neovide-specific settings
+-- neovide specific settings
 if vim.g.neovide ~= nil then
-  vim.o.guifont = "UbuntuMono Nerd Font:h26"
+  vim.o.guifont = "UbuntuMono Nerd Font:h22"
 
   -- Toggle fullscreen with F11 in Neovide
   vim.api.nvim_set_keymap('n', '<F11>', [[<Cmd>lua vim.g.neovide_fullscreen = not vim.g.neovide_fullscreen<CR>]], { noremap = true, silent = true })
@@ -11,7 +11,7 @@ if vim.g.neovide ~= nil then
   vim.keymap.set({ "n", "v" }, "<C-0>", ":lua vim.g.neovide_scale_factor = 1<CR>")
 end
 
--- goneovim-specific settings
+-- goneovim specific settings
 if vim.g.goneovim ~= nil then
   vim.o.guifont = "UbuntuMono Nerd Font:h20"
 end
