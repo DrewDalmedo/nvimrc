1eeab8b65cda48262cd1a47f9b2d6461418a6a15
Drew Dalmedo <andrewdalmedo@gmail.com>
2025-11-06 19:52:56 -0500

    custom tab labeling

--- options.lua
+++ options.lua
@@ -17,6 +17,25 @@ vim.opt.termguicolors = true
 -- allow project-level configurations
 vim.opt.exrc = true
 
+vim.o.showtabline = 1  -- Always show tabline (0=never, 1=if multiple tabs, 2=always)
+vim.o.tabline = '%!v:lua.Tabline()'
+
+function _G.Tabline()
+  local s = ''
+  for i = 1, vim.fn.tabpagenr('$') do
+    local winnr = vim.fn.tabpagewinnr(i)
+    local buflist = vim.fn.tabpagebuflist(i)
+    local bufnr = buflist[winnr]
+    local bufname = vim.fn.bufname(bufnr)
+    local tabnum = tostring(i)
+    local label = bufname ~= '' and vim.fn.fnamemodify(bufname, ':t') or '[No Name]'
+    local hl = i == vim.fn.tabpagenr() and '%#TabLineSel#' or '%#TabLine#'
+    s = s .. hl .. ' ' .. tabnum .. ': ' .. label .. ' '
+  end
+  s = s .. '%#TabLineFill#'
+  return s
+end
+
 -- if on Windows and bash is installed, set the default shell from cmd.exe to bash
 if vim.fn.has('win32') == 1 and vim.fn.executable('bash') == 1 then
   vim.opt.shell = 'bash'
