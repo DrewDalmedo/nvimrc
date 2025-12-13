99ed38f7347d80487f477bc268be69a78ea7e71e
Drew Dalmedo <andrewdalmedo@gmail.com>
2025-12-13 04:07:26 -0500

    feat(gui): add platform-specific font sizes

--- graphical.lua
+++ graphical.lua
@@ -1,6 +1,15 @@
 -- neovide specific settings
 if vim.g.neovide ~= nil then
-  vim.o.guifont = "UbuntuMono Nerd Font:h22"
+  -- default / fallback font size 
+  font_size = 22
+
+  if vim.fn.has("macunix") == 1 then
+    font_size = 26
+  elseif vim.fn.has("linux") == 1 then
+    font_size = 12
+  end
+
+  vim.o.guifont = "UbuntuMono Nerd Font:h" .. font_size
 
   -- Toggle fullscreen with F11 in Neovide
   vim.api.nvim_set_keymap('n', '<F11>', [[<Cmd>lua vim.g.neovide_fullscreen = not vim.g.neovide_fullscreen<CR>]], { noremap = true, silent = true })
