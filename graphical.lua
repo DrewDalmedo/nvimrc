6f3f5d4ae178c3760e36962dc5496ef36617df78
Drew Dalmedo <andrewdalmedo@gmail.com>
2026-02-19 13:48:54 -0500

    refactor(gui): set universal GUI font

--- graphical.lua
+++ graphical.lua
@@ -1,26 +1,14 @@
+-- universal
+vim.o.guifont = "UbuntuMono Nerd Font:h22"
+
 -- neovide specific settings
 if vim.g.neovide ~= nil then
-  -- default / fallback font size 
-  font_size = 22
-
-  if vim.fn.has("macunix") == 1 then
-    font_size = 26
-  elseif vim.fn.has("linux") == 1 then
-    font_size = 12
-  end
-
-  vim.o.guifont = "UbuntuMono Nerd Font:h" .. font_size
-
-  -- Toggle fullscreen with F11 in Neovide
+  -- toggle fullscreen with F11
   vim.api.nvim_set_keymap('n', '<F11>', [[<Cmd>lua vim.g.neovide_fullscreen = not vim.g.neovide_fullscreen<CR>]], { noremap = true, silent = true })
 
-  -- dynamic resizing
+  -- live resizing
   vim.keymap.set({ "n", "v" }, "<C-=>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>", { noremap = true, silent = true })
   vim.keymap.set({ "n", "v" }, "<C-->", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>", { noremap = true, silent = true })
   vim.keymap.set({ "n", "v" }, "<C-0>", ":lua vim.g.neovide_scale_factor = 1<CR>", { noremap = true, silent = true })
 end
 
--- goneovim specific settings
-if vim.g.goneovim ~= nil then
-  vim.o.guifont = "UbuntuMono Nerd Font:h20"
-end
